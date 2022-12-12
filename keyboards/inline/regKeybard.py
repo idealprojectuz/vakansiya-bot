267c99d68cddd329ed0b980d54479c30303e7360
Hayotbek <idealprojectuz@gmail.com>
2022-12-13 01:46:15 +0500

    first commit

--- keyboards/inline/regKeybard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.types.login_url import LoginUrl
-
-registration_menu=InlineKeyboardMarkup(
-    inline_keyboard=[
-    [
-        InlineKeyboardButton(text="Tizimga kirish", login_url=LoginUrl('https://xakaton.idealproject.uz/')),
-    ]
-])
